51c45cb17837b7b4cbb6ff8284e20fca389cfb05
Alex Thomas <alex.c.thomas@gmail.com>
2015-10-06 14:17:23 +0100

    baloo initial commit

---
+++ lib/baloo.rb
@@ -0,0 +1,5 @@
+require 'active_support/dependencies'
+
+module Baloo
+  autoload :Base,   'baloo/api_controller/base'
+end
---
+++ lib/tasks/baloo_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :baloo do
+#   # Task goes here
+# end
